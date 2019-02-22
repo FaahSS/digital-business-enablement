863e17947e888c823bcdc5a8214ce9e52eda54a7
Logon Aluno <logonrmlocal@fiap.com.br>
2019-02-22 11:34:55 -0300

    WS Provider

---
+++ 05-Loja-App-Web-SOAP/src/br/com/fiap/loja/bo/EstoqueBO.java
@@ -0,0 +1,20 @@
+package br.com.fiap.loja.bo;
+
+import org.apache.axis2.AxisFault;
+
+import br.com.fiap.loja.to.ProdutoTO;
+
+public class EstoqueBO {
+	public static ProdutoTO consultarProduto(int codigo) throws AxisFault{
+		if(codigo==401){
+			return new ProdutoTO(codigo, 2.99, 4, "Camiseta Branca" );
+		} else if(codigo==402) {
+			return new ProdutoTO(codigo, 2.99, 4, "Camiseta Azul" );
+		} else if(codigo==403) {
+			return new ProdutoTO(codigo, 2.99, 4, "Camiseta Rosa" );
+		} else {
+			throw new AxisFault("Produto não encontrado!");
+		}
+	}
+
+}
